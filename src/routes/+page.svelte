2774b4b9eabc03cdf62d8e018325a94526432685
Soraia F Paulo <soraiafpaulo@gmail.com>
2024-02-26 16:04:50 +0000

    Fix formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,9 @@
 	import type { Thread } from 'openai/resources/beta/index.mjs';
 	import type { FileObject } from 'openai/resources/index.mjs';
 	import { afterUpdate } from 'svelte';
-	
+
 	export let data;
-	
+
 	let patients: Patient[] = [];
 	let patient: Patient | undefined;
 	let previousPatientID: string | undefined;
@@ -24,29 +24,28 @@
 	let files: FileObject[] = [];
 	let thread: Thread | undefined;
 	let messages: Message[] = [];
-	
+
 	$: getUser();
-	
+
 	$: patients = data.assistants.map((assistant) => assistant.metadata);
-	
+
 	$: assistantWithPatient = data.assistants.find(
-	(assistant) => assistant.metadata.id === patient?.id
+		(assistant) => assistant.metadata.id === patient?.id
 	);
-	
+
 	$: if (patient) {
-		if(previousPatientID == undefined || previousPatientID != patient.id){
-			previousPatientID=patient.id;
+		if (previousPatientID == undefined || previousPatientID != patient.id) {
+			previousPatientID = patient.id;
 			readFiles();
 			createThread();
 		}
 	}
-	
-	
+
 	$: if (thread) {
 		readMessages();
-		insertThread();	
+		insertThread();
 	}
-	
+
 	let element: HTMLDivElement;
 	$: reversedMessages = messages.toReversed();
 	let message = '';
@@ -54,69 +53,76 @@
 
 	// Either afterUpdate()
 	afterUpdate(() => {
-		if(reversedMessages) scrollToBottom(element);
-  });
-	
-	$: if(reversedMessages && element) {
+		if (reversedMessages) scrollToBottom(element);
+	});
+
+	$: if (reversedMessages && element) {
 		scrollToBottom(element);
 	}
-	
+
 	const getUser = async () => {
 		let user = await data.supabase.auth.getUser();
 		userID = user.data.user?.id;
-	}
-	
+	};
+
 	const insertThread = async () => {
 		//await data.supabase.from('threads').upsert({ user_id: userID, thread_id: thread?.id, assistant_id: assistantWithPatient?.id},{onConflict: 'thread_id'});
-		
-		let threadsData = await data.supabase.from('threads').select().match({thread_id: thread?.id}).maybeSingle();
-		
-		if(!threadsData.data){
-			await data.supabase.from('threads').insert({ user_id: userID, thread_id: thread?.id, assistant_id: assistantWithPatient?.id});
+
+		let threadsData = await data.supabase
+			.from('threads')
+			.select()
+			.match({ thread_id: thread?.id })
+			.maybeSingle();
+
+		if (!threadsData.data) {
+			await data.supabase
+				.from('threads')
+				.insert({ user_id: userID, thread_id: thread?.id, assistant_id: assistantWithPatient?.id });
 		}
-		
-	}
-	
+	};
+
 	const logout = () => {
 		data.supabase.auth.signOut();
 	};
-	
+
 	const readFiles = async () => {
 		if (patient) {
 			files = data.files.filter((file) => assistantWithPatient?.file_ids.includes(file.id));
 		}
 	};
-	
+
 	const createThread = async () => {
-		
-		let threadsData = await data.supabase.from('threads').select('thread_id').match({user_id: userID, assistant_id: assistantWithPatient?.id}).maybeSingle();
-		
-		if(threadsData.data){
-			let threadDataID = threadsData.data.thread_id
+		let threadsData = await data.supabase
+			.from('threads')
+			.select('thread_id')
+			.match({ user_id: userID, assistant_id: assistantWithPatient?.id })
+			.maybeSingle();
+
+		if (threadsData.data) {
+			let threadDataID = threadsData.data.thread_id;
 			thread = await data.openai.beta.threads.retrieve(threadDataID);
-		}
-		else {
+		} else {
 			thread = await data.openai.beta.threads.create({});
 		}
 	};
-	
+
 	const readMessages = async () => {
 		if (thread) {
 			const messagesData = await data.openai.beta.threads.messages.list(thread.id);
 			messages = messagesData.data;
 		}
 	};
-	
+
 	const createMessage = async () => {
 		if (thread) {
 			const newMessage = message;
 			message = '';
 			messages = [
-			{
-				role: 'user',
-				content: [{ type: 'text', text: { value: newMessage, annotations: [] } }],
-			},
-			...messages,
+				{
+					role: 'user',
+					content: [{ type: 'text', text: { value: newMessage, annotations: [] } }],
+				},
+				...messages,
 			];
 			await data.openai.beta.threads.messages.create(thread.id, {
 				role: 'user',
@@ -124,7 +130,7 @@
 			});
 		}
 	};
-	
+
 	const createAndPollRun = async () => {
 		if (thread && assistantWithPatient) {
 			let runIsProcessing = true;
@@ -142,10 +148,9 @@
 		}
 	};
 
-	const scrollToBottom =  async (node: HTMLDivElement) => {
-		node.scroll({top: node.scrollHeight, behavior: 'smooth',});
+	const scrollToBottom = async (node: HTMLDivElement) => {
+		node.scroll({ top: node.scrollHeight, behavior: 'smooth' });
 	};
-
 </script>
 
 <div class="flex h-full flex-col">
@@ -162,39 +167,39 @@
 	<div class="flex flex-1 flex-row overflow-y-auto">
 		<div class="container w-96 py-6">
 			{#if patient}
-			<div class="flex flex-col gap-y-4">
+				<div class="flex flex-col gap-y-4">
+					<div class="flex flex-col items-center gap-y-4">
+						<Avatar.Root class="h-40 w-40">
+							<Avatar.Image src="{base}{patient.avatar}" alt="PT" />
+							<Avatar.Fallback>PT</Avatar.Fallback>
+						</Avatar.Root>
+						<PatientSelector {patients} bind:value={patient} />
+					</div>
+					<div class="mt-10 flex flex-col gap-y-2">
+						<div class="flex flex-row items-center gap-x-2 pb-2">
+							<span class="flex-1 font-semibold">FILES</span>
+							<Button variant="secondary" size="sm">
+								<Paperclip class="mr-2 h-3 w-3" />
+								Add file
+							</Button>
+						</div>
+						{#each files as file}
+							<div class="flex flex-row items-center gap-x-2">
+								<span class="flex-1 truncate text-sm">{file.filename}</span>
+								<Button size="iconsm" variant="outline">
+									<Download class="h-3 w-3" />
+								</Button>
+							</div>
+						{/each}
+					</div>
+				</div>
+			{:else}
 				<div class="flex flex-col items-center gap-y-4">
-					<Avatar.Root class="h-40 w-40">
-						<Avatar.Image src="{base}{patient.avatar}" alt="PT" />
-						<Avatar.Fallback>PT</Avatar.Fallback>
-					</Avatar.Root>
+					<span class="text-center text-sm text-muted-foreground">
+						Please select a patient to start a conversation.
+					</span>
 					<PatientSelector {patients} bind:value={patient} />
 				</div>
-				<div class="mt-10 flex flex-col gap-y-2">
-					<div class="flex flex-row items-center gap-x-2 pb-2">
-						<span class="flex-1 font-semibold">FILES</span>
-						<Button variant="secondary" size="sm">
-							<Paperclip class="mr-2 h-3 w-3" />
-							Add file
-						</Button>
-					</div>
-					{#each files as file}
-					<div class="flex flex-row items-center gap-x-2">
-						<span class="flex-1 truncate text-sm">{file.filename}</span>
-						<Button size="iconsm" variant="outline">
-							<Download class="h-3 w-3" />
-						</Button>
-					</div>
-					{/each}
-				</div>
-			</div>
-			{:else}
-			<div class="flex flex-col items-center gap-y-4">
-				<span class="text-center text-sm text-muted-foreground">
-					Please select a patient to start a conversation.
-				</span>
-				<PatientSelector {patients} bind:value={patient} />
-			</div>
 			{/if}
 		</div>
 		<Separator orientation="vertical" />
@@ -202,79 +207,80 @@
 			<div class="container flex flex-row items-center gap-x-2 py-4">
 				<span class="text-xs font-bold">CONVERSATION</span>
 				<span class="flex-1 text-xs text-muted-foreground"
-				>{thread?.id ?? 'Select a patient to start conversation'}</span
+					>{thread?.id ?? 'Select a patient to start conversation'}</span
 				>
 				<Button
-				variant="secondary"
-				on:click={() => {
-					messages = [];
-					//createThread();
-				}}
+					variant="secondary"
+					on:click={() => {
+						messages = [];
+						//createThread();
+					}}
 				>
-				<Eraser class="mr-2 h-4 w-4" />
-				Clear
-			</Button>
-			<LogsDialog />
-		</div>
-		<div bind:this={element}
-		class="mx-auto flex w-full max-w-[1000px] flex-1 flex-col gap-y-4 overflow-y-auto px-6 py-4"
-		>
-		{#each reversedMessages as message}
-		<div
-		class:self-end={message.role === 'user'}
-		class="flex flex-col gap-y-1"
-		class:items-end={message.role === 'user'}
-		>
-		<span class="text-xs font-bold">{message.role.toUpperCase()}</span>
-		{#each message.content as contentItem}
-		{#if contentItem.type === 'text'}
-		<p class="max-w-[400px] text-sm">{contentItem.text.value}</p>
-		{:else}
-		<!-- else content here -->
-		{/if}
-		{/each}
-	</div>
-	{/each}
-</div>
-<div class="mx-auto flex w-full max-w-[1000px] flex-col items-center gap-y-4 px-6 py-5">
-	<Card class="flex w-full flex-col gap-y-4 px-5 py-5">
-		<textarea
-		placeholder="Enter your message..."
-		class="resize-none border-none outline-none"
-		bind:value={message}
-		/>
-		<div class="flex flex-row items-center justify-between">
-			<Button
-			size="icon"
-			variant="secondary"
-			on:click={() => {
-				message = '';
-			}}
+					<Eraser class="mr-2 h-4 w-4" />
+					Clear
+				</Button>
+				<LogsDialog />
+			</div>
+			<div
+				bind:this={element}
+				class="mx-auto flex w-full max-w-[1000px] flex-1 flex-col gap-y-4 overflow-y-auto px-6 py-4"
 			>
-			<Eraser class="h-4 w-4" />
-		</Button>
-		<Button
-		disabled={!message || running}
-		on:click={async () => {
-			running = true;
-			await createMessage();
-			await createAndPollRun();
-			running = false;
-		}}
-		>
-		Send
-		{#if running}
-		<Loader2 class="ml-2 h-4 w-4 animate-spin" />
-		{:else}
-		<SendHorizontal class="ml-2 h-4 w-4" />
-		{/if}
-	</Button>
-</div>
-</Card>
-<span class="text-sm text-muted-foreground">
-	This conversation is being recorded anonymously.
-</span>
-</div>
-</div>
-</div>
+				{#each reversedMessages as message}
+					<div
+						class:self-end={message.role === 'user'}
+						class="flex flex-col gap-y-1"
+						class:items-end={message.role === 'user'}
+					>
+						<span class="text-xs font-bold">{message.role.toUpperCase()}</span>
+						{#each message.content as contentItem}
+							{#if contentItem.type === 'text'}
+								<p class="max-w-[400px] text-sm">{contentItem.text.value}</p>
+							{:else}
+								<!-- else content here -->
+							{/if}
+						{/each}
+					</div>
+				{/each}
+			</div>
+			<div class="mx-auto flex w-full max-w-[1000px] flex-col items-center gap-y-4 px-6 py-5">
+				<Card class="flex w-full flex-col gap-y-4 px-5 py-5">
+					<textarea
+						placeholder="Enter your message..."
+						class="resize-none border-none outline-none"
+						bind:value={message}
+					/>
+					<div class="flex flex-row items-center justify-between">
+						<Button
+							size="icon"
+							variant="secondary"
+							on:click={() => {
+								message = '';
+							}}
+						>
+							<Eraser class="h-4 w-4" />
+						</Button>
+						<Button
+							disabled={!message || running}
+							on:click={async () => {
+								running = true;
+								await createMessage();
+								await createAndPollRun();
+								running = false;
+							}}
+						>
+							Send
+							{#if running}
+								<Loader2 class="ml-2 h-4 w-4 animate-spin" />
+							{:else}
+								<SendHorizontal class="ml-2 h-4 w-4" />
+							{/if}
+						</Button>
+					</div>
+				</Card>
+				<span class="text-sm text-muted-foreground">
+					This conversation is being recorded anonymously.
+				</span>
+			</div>
+		</div>
+	</div>
 </div>
