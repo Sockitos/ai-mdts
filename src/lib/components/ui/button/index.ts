0f8e7606b33e8c126e6e1337c3e0d7b16a5ae084
Sockitos <jrnogueira@edu.ulisboa.pt>
2024-04-18 00:49:39 +0100

    Update dependencies and UI kit

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -1,9 +1,9 @@
 import type { Button as ButtonPrimitive } from 'bits-ui';
-import { tv, type VariantProps } from 'tailwind-variants';
+import { type VariantProps, tv } from 'tailwind-variants';
 import Root from './button.svelte';
 
 const buttonVariants = tv({
-	base: 'inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+	base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
 	variants: {
 		variant: {
 			default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
@@ -20,7 +20,6 @@ const buttonVariants = tv({
 			lg: 'h-10 rounded-md px-8',
 			icon: 'h-9 w-9',
 			iconsm: 'h-8 w-8',
-			iconlg: 'h-10 w-10',
 		},
 	},
 	defaultVariants: {
